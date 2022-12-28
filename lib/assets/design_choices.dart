925e4a8ed1148668b7290bd50a2f7fbccb91ffbc
Alexandre Moreira <alexrmoreira777@gmail.com>
2022-12-28 07:25:54 -0300

    Fix Const

--- lib/assets/design_choices.dart
+++ lib/assets/design_choices.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DesignChoice {
-  static Color grayColor = Color.fromARGB(255, 235, 235, 235);
-  static Color grayText = Color.fromARGB(255, 93, 92, 92);
+  static Color grayColor = const Color.fromARGB(255, 235, 235, 235);
+  static Color grayText = const Color.fromARGB(255, 93, 92, 92);
 
   static double titleFontSize = 18;
   static double titleIconSize = 10;
 
   static FontWeight titleFontWeight = FontWeight.w500;
   static FontWeight buttonFontWeight = FontWeight.w500;
-  static Color navBarNotSelectedColor = Color.fromARGB(255, 198, 198, 198);
+  static Color navBarNotSelectedColor =
+      const Color.fromARGB(255, 198, 198, 198);
 }
